0bee9f14e493060724d7fa245d3ece38d2712a25
g4iliunas <gailiunaslaurynas@tutanota.com>
2024-12-24 21:41:25 +0200

    fix: typo

--- database/gen_provider.py
+++ database/gen_provider.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     # generate a token
     token = hashlib.sha256((username + str(time.time())).encode()).hexdigest() 
 
-    # open a connect to redis db
+    # open a connection to redis db
     r = redis.Redis(host='localhost', port=6379, db=0)
 
     # add an entry (we dont include the username or balance in here, because other tables will hold this entry)
     r.hset(f"providers:{token}", mapping={"bytes_sent": 0, "bytes_received": 0})
 
     # print out the result
-    print(f"providers:{token} :", r.hgetall(f"providers:{token}"))
+    print(f"providers:{token}", ":", r.hgetall(f"providers:{token}"))
