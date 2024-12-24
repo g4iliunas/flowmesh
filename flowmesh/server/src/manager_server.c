a602b65a9b7838260a5ed1fc1c45d1e3f9d300ef
g4iliunas <gailiunaslaurynas@tutanota.com>
2024-12-24 11:28:26 +0200

    fix: typo

--- flowmesh/server/src/manager_server.c
+++ flowmesh/server/src/manager_server.c
@@ -100,20 +100,20 @@ static void on_new_connection(uv_stream_t *server, int status)
 uv_tcp_t *fm_manager_server_init(uv_loop_t *loop, const char *address,
                                  const uint16_t port)
 {
-    uv_tcp_t *proxy_server = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
-    if (!proxy_server)
+    uv_tcp_t *manager_server = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
+    if (!manager_server)
         return NULL;
 
-    uv_tcp_init(loop, proxy_server);
+    uv_tcp_init(loop, manager_server);
     struct sockaddr_in sin = {0};
     uv_ip4_addr(address, port, &sin);
-    uv_tcp_bind(proxy_server, (struct sockaddr *)&sin, 0);
+    uv_tcp_bind(manager_server, (struct sockaddr *)&sin, 0);
 
-    if (uv_listen((uv_stream_t *)proxy_server, SOMAXCONN, on_new_connection) !=
-        0) {
-        free(proxy_server);
+    if (uv_listen((uv_stream_t *)manager_server, SOMAXCONN,
+                  on_new_connection) != 0) {
+        free(manager_server);
         return NULL;
     }
 
-    return proxy_server;
+    return manager_server;
 }
