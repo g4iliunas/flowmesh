e2b96497c299770bb5da18ba80ed7a984c0e1dc2
g4iliunas <gailiunaslaurynas@tutanota.com>
2024-12-24 21:36:16 +0200

    feat: provider entry gen

---
+++ database/gen_provider.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import redis
+import sys
+import hashlib
+import time
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./gen_provider.py <username>")
+
+    username = sys.argv[1]
+
+    # generate a token
+    token = hashlib.sha256((username + str(time.time())).encode()).hexdigest() 
+
+    # open a connect to redis db
+    r = redis.Redis(host='localhost', port=6379, db=0)
+
+    # add an entry (we dont include the username or balance in here, because other tables will hold this entry)
+    r.hset(f"providers:{token}", mapping={"bytes_sent": 0, "bytes_received": 0})
+
+    # print out the result
+    print(f"providers:{token} :", r.hgetall(f"providers:{token}"))
