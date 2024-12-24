85f5ce3c28526ecb04c0c5121f82f1667201fd22
g4iliunas <gailiunaslaurynas@tutanota.com>
2024-12-24 21:49:40 +0200

    feat: hashing the token

--- database/gen_provider.py
+++ database/gen_provider.py
@@ -13,12 +13,16 @@ if __name__ == "__main__":
 
     # generate a token
     token = hashlib.sha256((username + str(time.time())).encode()).hexdigest() 
+    print("Generated token:", token)
+
+    # not hashing it would be a security flaw
+    hashed_token = hashlib.sha256(token.encode()).hexdigest()
 
     # open a connection to redis db
     r = redis.Redis(host='localhost', port=6379, db=0)
 
     # add an entry (we dont include the username or balance in here, because other tables will hold this entry)
-    r.hset(f"providers:{token}", mapping={"bytes_sent": 0, "bytes_received": 0})
+    r.hset(f"providers:{hashed_token}", mapping={"bytes_sent": 0, "bytes_received": 0})
 
     # print out the result
-    print(f"providers:{token}", ":", r.hgetall(f"providers:{token}"))
+    print(f"providers:{hashed_token}", ":", r.hgetall(f"providers:{hashed_token}"))
